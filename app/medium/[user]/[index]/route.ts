44429cfab3aad88ad0fedce6c93dd4b4cb1a596c
Iman Tumorang <iman@tumorang.com>
2024-02-12 19:50:57 +0800

    chore: fix redirect if not img request

--- app/medium/[user]/[index]/route.ts
+++ app/medium/[user]/[index]/route.ts
@@ -7,22 +7,30 @@ export async function GET(req: Request) {
   var pathName = reqURL.pathname.split('/');
   const idx = pathName.pop(); // Get the last segment of the path
   const username = pathName.pop(); // Get the username
-
   // @ts-ignore
   const { title, thumbnail, url, date, description } = await getArticle(idx, username);
+  const dest = headers.get('sec-fetch-dest') || headers.get('Sec-Fetch-Dest');
+  const accept = headers.get('accept');
+  const isImage = dest ? dest === 'image' : !/text\/html/.test(accept);
 
-  const svgContent = medium({
-    title,
-    thumbnail,
-    url,
-    date,
-    description,
-  });
+  if (isImage) {
+    // Generate the SVG content
+    const svgContent = medium({
+      title,
+      thumbnail,
+      url,
+      date,
+      description,
+    });
 
-  return new Response(svgContent, {
-    headers: {
-      'Cache-Control': 's-maxage=3600, stale-while-revalidate',
-      'Content-Type': 'image/svg+xml',
-    },
-  });
+    return new Response(svgContent, {
+      headers: {
+        'Cache-Control': 's-maxage=3600, stale-while-revalidate',
+        'Content-Type': 'image/svg+xml',
+      },
+    });
+  }
+
+  // Redirect to the URL if not an image request
+  return Response.redirect(url, 301);
 }
